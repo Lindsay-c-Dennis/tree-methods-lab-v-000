81904e2eaaa2f0b89b2da2d57c70c8f334e5a83e
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-07-12 21:09:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function findOrAdd(rootNode, newNode) {
     if (rootNode.left) {
       findOrAdd(rootNode.left, newNode);
     } else {
-      return currentNode.left = newNode
+      return rootNode.left = newNode;
     }  
   } else if ()
 }
